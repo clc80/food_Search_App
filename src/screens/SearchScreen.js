fd73394e984aba5cb2b2f61dfd232c8b000d5afc
clc80 <clc_80@yahoo.com>
2019-09-03 23:01:41 -0700

    added new navigator & first screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default SearchScreen;
