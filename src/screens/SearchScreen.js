17250cc34af228524913eeab91512e8305b194b2
clc80 <clc_80@yahoo.com>
2019-09-08 21:45:03 -0700

    Added useEffect hook to get data when app starts

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm ] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
-      <Text>Search Screen</Text>
       <SearchBar
         term={term}
         onTermChange={ newTerm => setTerm(newTerm) }
-        onTermSubmit={() => console.log("term was submitted")}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text> {term}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null }
+      <Text> We have found {results.length} results</Text>
     </View>
   );
 }
