d4afd0f7aecbc422ed621c80009d135364bb2238
clc80 <clc_80@yahoo.com>
2019-09-08 23:12:37 -0700

    added new screen for full page of each restaurant

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const ResultsShowScreen = () => {
+
+  return (
+    <View>
+      <Text> Results Show </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default ResultsShowScreen;
